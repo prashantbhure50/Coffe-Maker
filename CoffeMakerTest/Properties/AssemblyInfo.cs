e0dac6a578ad642d7075fccfd0205bb309e0311d
Prashant Bhure <Prashantbhure50@gmail.com>
2021-04-27 21:26:36 +0530

    [file is added]

---
+++ CoffeMakerTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CoffeMakerTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CoffeMakerTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b213f14c-0d91-46f5-a9d9-6c5281e86731")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CoffeMakerTest/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using CoffeMaker;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+
+namespace CoffeMakerTest
+{
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void OrderACoffee_Should_return_Received_Message()
+        {
+            //Arrange
+            StarbuckStore store = new StarbuckStore(new FakeStarbucksStore());
+            //Act
+            string result = store.OrderCoffee(2, 4);
+            Assert.AreEqual("Your Order is received.", result);
+        }
+
+        [TestMethod]
+        public void OrderACoffee_Should_Return_Received_MessageUsingStub()
+        {
+            StarbuckStore store = new StarbuckStore(new StubStarbucks());
+            string result = store.OrderCoffee(2, 4);
+            Assert.AreEqual("Your order is received.", result);
+        }
+
+        //Moq MOCK
+
+        [TestMethod]
+        public void OrderACoffee_Should_Return_Received_MessageUsingMock()
+        {
+            var service = new Mock<IMakeACoffee>();
+            service.Setup(x => x.CheckIngredientAvailability()).Returns(true);
+            var store = new StarbuckStore(new Starbucks);
+            var result = store.OrderCoffee(2, 4);
+            Assert.AreEqual("Your Order is received", result);
+
+        }
+
+
+
+    }
+
+}
